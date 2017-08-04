80fdfb4b8e1222cb8c82222b6013a4265dd4916d
Dmitriy <dmt.tym@gmail.com>
2017-08-04 16:21:28 +0300

    Tasks 1-17 done. Tasks 18-23 should to be checked

--- ht_python.py
+++ ht_python.py
@@ -1,4 +1,4 @@
-import math
+import math, random
 from datetime import datetime,date
 #
 # print("------------------------------------")
@@ -150,82 +150,187 @@ from datetime import datetime,date
 #     print("%d is even" % number)
 #
 #
-print("------------------------------------")
-print("Task 15")
-print("------------------------------------")
-
-
-def circles (x1, y1, r1, x2, y2, r2):
-    circle_lenght = math.sqrt((x2-x1)**2+(y2-y1)**2)
-    if (r2+r1) >= circle_lenght and abs((r1-r2)) <= circle_lenght:
-        return True
-    else:
-        return False
-
-x1 = 1
-y1 = 4
-r1 = 1
-
-x2 = 1
-y2 = 2
-r2 = 1
-
-
-if circles (x1, y1, r1, x2, y2, r2):
-    print("Circles cross ")
-else:
-    print("Circles don't cross")
-
-
-print("------------------------------------")
-print("Task 16")
-print("------------------------------------")
-
-v1 = int(input("Ведите скорость первого поезда:"))
-v2 = int(input("Ведите скорость второго поезда:"))
-
-def trains_not_crush (v1, v2, s=10, s1=4):
-    if s1/v1 <= (s-s1)/v2:
-        return True
-    else:
-        return False
-#
-# v1 = 100
-# v2 = 11
-
-if trains_not_crush (v1,v2):
-    print("It was close")
-else:
-    print("Crush")
-
-
-print("------------------------------------")
-print("Task 17")
-print("------------------------------------")
-
-def quadratic_equation (a, b, c):
-    D = b**2 - 4*a*c
-    if D > 0:
-        x1 = (-b + math.sqrt(D)) / (2 * a)
-        x2 = (-b - math.sqrt(D)) / (2 * a)
-        return round(x1, 3), round(x2, 3)
-    if D == 0:
-        x1 = x2 = -b / (2*a)
-        return round(x1, 3), round(x2, 3)
-    else:
-        return "Has no solution"
-
-a = float(input("Enter a value for a:"))
-b = float(input("Enter a value for b:"))
-c = float(input("Enter a value for c:"))
-
-result = quadratic_equation(a,b,c)
-print("\nFor current quadratic_equation:", result)
-
 # print("------------------------------------")
-# print("Task 18")
+# print("Task 15")
 # print("------------------------------------")
 #
-# def sum_of_numbers (a, b):
-#     subm = 0
-#     for i in range (a,b+1):
+#
+# def circles (x1, y1, r1, x2, y2, r2):
+#     circle_lenght = math.sqrt((x2-x1)**2+(y2-y1)**2)
+#     if (r2+r1) >= circle_lenght and abs((r1-r2)) <= circle_lenght:
+#         return True
+#     else:
+#         return False
+#
+# x1 = 1
+# y1 = 4
+# r1 = 1
+#
+# x2 = 1
+# y2 = 2
+# r2 = 1
+#
+#
+# if circles (x1, y1, r1, x2, y2, r2):
+#     print("Circles cross ")
+# else:
+#     print("Circles don't cross")
+#
+#
+# print("------------------------------------")
+# print("Task 16")
+# print("------------------------------------")
+#
+# v1 = int(input("Ведите скорость первого поезда:"))
+# v2 = int(input("Ведите скорость второго поезда:"))
+#
+# def trains_not_crush (v1, v2, s=10, s1=4):
+#     if s1/v1 <= (s-s1)/v2:
+#         return True
+#     else:
+#         return False
+#
+# if trains_not_crush (v1,v2):
+#     print("\nIt was close")
+# else:
+#     print("\nCrush")
+#
+#
+# print("------------------------------------")
+# print("Task 17")
+# print("------------------------------------")
+#
+# def quadratic_equation (a, b, c):
+#     D = b**2 - 4*a*c
+#     if D > 0:
+#         x1 = (-b + math.sqrt(D)) / (2 * a)
+#         x2 = (-b - math.sqrt(D)) / (2 * a)
+#         return round(x1, 3), round(x2, 3)
+#     if D == 0:
+#         x1 = x2 = -b / (2*a)
+#         return round(x1, 3), round(x2, 3)
+#     else:
+#         return "Has no solution"
+#
+# a = float(input("Enter a value for a:"))
+# b = float(input("Enter a value for b:"))
+# c = float(input("Enter a value for c:"))
+#
+# result = quadratic_equation(a,b,c)
+# print("\nFor current quadratic_equation:", result)
+
+print("------------------------------------")
+print("Task 18")
+print("------------------------------------")
+
+symbol1 = ord(input("Please write first symbol: "))
+symbol2 = ord(input("Please write second symbol: "))
+
+def total_sum_symbols (a, b):
+    symb_sum = 0
+    for i in range (a,b+1):
+        symb_sum += i
+    print("Total sum of Unicode is: ", symb_sum)
+
+
+if symbol1 <= symbol2:
+    total_sum_symbols(symbol1, symbol2)
+else:
+    total_sum_symbols(symbol2, symbol1)
+
+print("------------------------------------")
+print("Task 19")
+print("------------------------------------")
+
+
+range_num = int(input("Enter number of random numbers: "))
+random_num = int(input("Enter the maximum value for the range: "))
+
+
+def search_of_arithmetic_mean (range_num, random_num):
+    total_sum = 0
+    for i in range(range_num):
+        number = random.randint(0, random_num)
+        total_sum += number
+    arithmetic_mean = total_sum/2
+    return arithmetic_mean
+
+# result = (search_of_arithmetic_mean(range_num, random_num))
+# print("The arithmetic mean for %d randomly generated numbers between 0 and %d is: %.2f" % (range_num, random_num, search_of_arithmetic_mean(range_num, random_num)))
+
+
+print("------------------------------------")
+print("Task 20")
+print("------------------------------------")
+
+lst_begin = int(input("Enter lower bound of your list: "))
+lst_end = int(input("Enter upper bound of your list: "))
+
+def difference_between_odds_evens (lst_begin, lst_end):
+    lst = list(range(lst_begin,lst_end+1))
+    print(lst)
+    odds_sum = 0
+    even_sum = 0
+    for i in range (lst_begin, lst_end+1):
+        if i%2==0:
+            even_sum += i
+        else:
+            odds_sum += i
+    return even_sum-odds_sum
+
+print(difference_between_odds_evens (lst_begin, lst_end))
+
+print("------------------------------------")
+print("Task 21")
+print("------------------------------------")
+
+def is_included (numbers, basis):
+    while numbers >=1:
+        digit = numbers%10
+        if digit == basis:
+            return True
+        numbers //= 10
+    return False
+
+numbers = 1000
+digits1 = 1
+digits2 = 7
+
+for i in range(1,numbers+1):
+    if is_included(i,digits1) and is_included(i,digits2):
+        print (i)
+
+print("------------------------------------")
+print("Task 22")
+print("------------------------------------")
+
+def sum_exponentiation (number=3,boundary_value=1000000):
+    sum_number = 0
+    i = 0
+    while sum_number < boundary_value:
+        sum1 = number**i
+        i += 1
+        if sum1 <= boundary_value:
+            sum_number += sum1
+        else:
+            break
+    return sum_number
+
+print("Total sum of numbers, which are a power of 3 is:", sum_exponentiation())
+
+print("------------------------------------")
+print("Task 23")
+print("------------------------------------")
+
+def max_digit_in_number ():
+    lst = random.randint(1000000000000, 9999999999999)
+    print(lst)
+    max_number = max([int(i) for i in str(lst)])
+    return max_number, lst
+
+print("Max digit is %d in value %d" % max_digit_in_number())
+
+# print("------------------------------------")
+# print("Task 24")
+# print("------------------------------------")
+
